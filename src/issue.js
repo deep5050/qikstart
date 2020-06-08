01f4ff7541be0b7ee1f580c7d62af25ac3799815
Dipankar Pal <dipankarpal5050@gmail.com>
2020-06-08 05:32:26 +0000

    fix: old version updated

--- src/issue.js
+++ src/issue.js
@@ -1,36 +1,66 @@
-module.exports.en = () => {
-  return `**Note: This repository's issues are reserved for feature requests, bug reports
-and support questions.**
+module.exports.bug = () => {
+  return `---
+name: Bug report
+about: Create a report to help us improve
+title: ''
+labels: ''
+assignees: ''
 
-* **I'm submitting a ...**
-  + [ ] bug report
-  + [ ] feature request
-  + [ ] support request
+---
 
+**Describe the bug**
+A clear and concise description of what the bug is.
 
-* **Do you want to request a *feature*, report a *bug* or do you need *support*?**
+**To Reproduce**
+Steps to reproduce the behavior:
+1. Go to '...'
+2. Click on '....'
+3. Scroll down to '....'
+4. See error
 
+**Expected behavior**
+A clear and concise description of what you expected to happen.
 
+**Screenshots**
+If applicable, add screenshots to help explain your problem.
 
-* **What is the current behavior?**
+**Desktop (please complete the following information):**
+ - OS: [e.g. iOS]
+ - Browser [e.g. chrome, safari]
+ - Version [e.g. 22]
 
+**Smartphone (please complete the following information):**
+ - Device: [e.g. iPhone6]
+ - OS: [e.g. iOS8.1]
+ - Browser [e.g. stock browser, safari]
+ - Version [e.g. 22]
 
+**Additional context**
+Add any other context about the problem here.
 
-* **If the current behavior is a bug, please provide the steps to reproduce and if possible a minimal demo of the problem.**
+`;
+}
 
+module.exports.feature=()=>{
+    return `---
+name: Feature request
+about: Suggest an idea for this project
+title: ''
+labels: ''
+assignees: ''
 
+---
 
-* **What is the expected behavior?**
+**Is your feature request related to a problem? Please describe.**
+A clear and concise description of what the problem is. Ex. I'm always frustrated when [...]
 
+**Describe the solution you'd like**
+A clear and concise description of what you want to happen.
 
+**Describe alternatives you've considered**
+A clear and concise description of any alternative solutions or features you've considered.
 
-* **What is the motivation / use case for changing the behavior?**
-
-
-
-* **Please describe your environment (your operating system and version):**
-
-
-
+**Additional context**
+Add any other context or screenshots about the feature request here.
 `;
 }
